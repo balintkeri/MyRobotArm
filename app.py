9be7583504af4e6a7364850643eaa950f956cde0
balintkeri <balintkeri0515@gmail.com>
2025-09-13 18:37:00 +0200

    angle input iterations

--- app.py
+++ app.py
@@ -30,14 +30,16 @@ class App:
         self.angleLabel.grid(row=0, column=1, padx=5, pady=5)
 
 
+        self.angleEntries = []
         for i in self.robot_arm.controller.nodes:
             
             node_id = i.id
-            node_entry = tk.Entry(self.anglegridFrame, width=10)
+            node_entry = tk.Label(self.anglegridFrame, width=10, text=str(node_id))
             node_entry.insert(0, str(node_id))
             node_entry.grid(row=node_id, column=0, padx=5, pady=5)
             angle_entry = tk.Entry(self.anglegridFrame, width=10)
             angle_entry.grid(row=node_id, column=1, padx=5, pady=5)
+            self.angleEntries.append(angle_entry)
 
         self.sendCommandButton = tk.Button(self.controlFrame, text="Send Command", command=self.sendCommand)
         self.sendCommandButton.pack(pady=10)
@@ -69,10 +71,13 @@ class App:
     def sendCommand(self):
         try:
             if self.robot_arm.isControlType("AngleControl"):
-                angle = float(self.angleEntry.get())
-                node = int(self.nodeEntry.get())
-                self.robot_arm.command(angle=angle, node=node)
-                print(f"Sent Angle Control Command: Node {node}, Angle {angle}")
+                for i, entry in enumerate(self.angleEntries):
+                    angle_str = entry.get()
+                    if angle_str:
+                        angle = float(angle_str)
+                        node_id = self.robot_arm.controller.nodes[i].id
+                        self.robot_arm.command(angle=angle, node=node_id)
+                        print(f"Sent Angle Control Command: Node {node_id}, Angle {angle}")
             else:
                 position_str = self.positionEntry.get()
                 x, y, z = map(float, position_str.split(','))
