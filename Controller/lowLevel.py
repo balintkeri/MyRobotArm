b521121725ec6cd12fe8da6e82c22b4b2660d3cf
balintkeri <balintkeri0515@gmail.com>
2025-09-13 17:19:09 +0200

    Node added

---
+++ Controller/lowLevel.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+
+
+
+class Node:
+    def __init__(self, id: int):
+        self.id = id
+        self._setup()
+
+    def _setup(self):
+        GPIO.setup(self.id, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.id, 50)
+        self.pwm.start(0)
+
+    def _cleanup(self):
+        self.pwm.stop()
+        GPIO.cleanup(self.id)
+
+    def set_angle(self, angle: float):
+        duty = angle / 18 + 2
+        self.pwm.ChangeDutyCycle(duty)
+        time.sleep(1)
+        self.pwm.ChangeDutyCycle(0)
+
+
+class CurrentSensor:
+    def __init__(self, id: int):
+        self.id = id
+        self._setup()
+
+    def _setup(self):
+        GPIO.setup(self.id, GPIO.IN)
+
+    def read_value(self) -> int:
+        return GPIO.input(self.id)
+
+    def _cleanup(self):
+        GPIO.cleanup(self.id)
+
