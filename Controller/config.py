17f3865dae15f24c97191679a3cb7f84e4823fda
balintkeri <balintkeri0515@gmail.com>
2025-09-28 20:19:25 +0200

    config order modified

--- Controller/config.py
+++ Controller/config.py
@@ -8,7 +8,7 @@ class NodeConfig:
     initAngle: float = 0
 
 servo_pins = [
-    NodeConfig(id = 32,  initAngle=90),
     NodeConfig(id = 31, stopActuation=False,),
+    NodeConfig(id = 32,  initAngle=90),
     NodeConfig(id = 33, initAngle=90),
 ]  # GPIO pins for servos
