c045167c5ba3f0b16877c093dfa5eb26f1138822
balintkeri <balintkeri0515@gmail.com>
2025-11-06 18:23:26 +0100

    magnet positioner added

--- Controller/config.py
+++ Controller/config.py
@@ -11,6 +11,7 @@ servo_pins = [
     NodeConfig(id = 31, stopActuation=False,),
     NodeConfig(id = 32,  initAngle=90),
     NodeConfig(id = 33, initAngle=90),
+    NodeConfig(id = 16, initAngle=90),
 ]  # GPIO pins for servos
 
 magnet_pin = 29  # GPIO pin for magnet control
